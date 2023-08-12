ec3ad239a38fee9fbf9effca88ec03f506179a5a
RukeshB <rukeshbasukala@gmail.com>
2023-08-12 23:11:20 +0545

    complete social link design

--- src/components/common/SocialLink.js
+++ src/components/common/SocialLink.js
@@ -6,36 +6,36 @@ const SocialLink = () => {
   return (
     <div className="flex flex-col fixed left-0 top-[35%]">
       <ul>
-        <li className="ml-[-100px] hover:ml-[0px] duration-150 w-[160px] h-[60px]">
+        <li className="ml-[-100px] hover:ml-[0px] duration-150 w-[160px] h-[60px] bg-gray-800">
           <a
-            href="#"
+            href="https://github.com/RukeshB"
             className="w-full p-4 flex justify-between items-center text-white"
           >
             Github <SiGithub size={30} />
           </a>
         </li>
 
-        <li className="ml-[-100px] hover:ml-[0px] duration-150 w-[160px] h-[60px]">
+        <li className="ml-[-100px] hover:ml-[0px] duration-150 w-[160px] h-[60px] bg-blue-600">
           <a
-            href="#"
+            href="https://www.linkedin.com/in/rukesh-basukala-849042197/"
             className="w-full p-4 flex justify-between items-center text-white"
           >
             Linkedin <SiLinkedin size={30} />
           </a>
         </li>
 
-        <li className="ml-[-100px] hover:ml-[0px] duration-150 w-[160px] h-[60px]">
+        <li className="ml-[-100px] hover:ml-[0px] duration-150 w-[160px] h-[60px] bg-orange-700">
           <a
-            href="#"
+            href="mailto:rukeshbasukala@gmail.com"
             className="w-full p-4 flex justify-between items-center text-white"
           >
             Email <HiOutlineMail size={30} />
           </a>
         </li>
 
-        <li className="ml-[-100px] hover:ml-[0px] duration-150 w-[160px] h-[60px]">
+        <li className="ml-[-100px] hover:ml-[0px] duration-150 w-[160px] h-[60px] bg-green-700">
           <a
-            href="#"
+            href="https://github.com/RukeshB"
             className="w-full p-4 flex justify-between items-center text-white"
           >
             Resume <BsPersonLinesFill size={30} />
