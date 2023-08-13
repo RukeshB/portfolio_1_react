50bf8348e4eb0494c87e2402dba24f0e5474f2c1
RukeshB <rukeshbasukala@gmail.com>
2023-08-13 23:47:26 +0545

    add content and design home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,33 @@
-const Home = () => {
-    return (
-        <div className="w-full h-screen bg-[#263A5F]">
+import { HiArrowNarrowRight } from "react-icons/hi";
 
+const Home = () => {
+  return (
+    <div name="home" className="w-full h-screen bg-[#263A5F]">
+      {/* container */}
+      <div className="max-w-[1000px] h-full flex flex-col justify-center mx-auto px-8 ">
+        <p className="text-[#FDB515]">Hi, my name is</p>
+        <h1 className="text-[#ccd6f6] font-bold text-4xl md:text-7xl">
+          Rukesh Basukala
+        </h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+          I am a full stack developer
+        </h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
+          I’m a full-stack developer specializing in building (and occasionally
+          designing) exceptional digital experiences. Currently, I’m focused on
+          building responsive full-stack web applications.
+        </p>
+        <div>
+          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#FDB515] hover:border-[#FDB515]">
+            View Work
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-3 " />
+            </span>
+          </button>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
