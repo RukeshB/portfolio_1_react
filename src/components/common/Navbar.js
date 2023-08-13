f4aa20073121a3fba5badf5647d055be77c00e37
RukeshB <rukeshbasukala@gmail.com>
2023-08-13 23:31:29 +0545

    navbar styling and fixing code

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -6,43 +6,63 @@ import SocialLinkMenu from "./SocialLinkMenu";
 const Navbar = () => {
   const [mobileNav, setMobileNav] = useState(false);
   const handleClick = () => setMobileNav(!mobileNav);
+  const navItem = ["Home", "About", "Work", "Contact"];
+
+  const navList = navItem.map((item) => {
+    return (
+      <li
+        className="hover:text-[#FDB515] duration-200 cursor-pointer"
+        key={item}
+      >
+        {item}
+      </li>
+    );
+  });
+
+  const mobileNavList = navItem.map((item) => {
+    return (
+      <li
+        className="hover:text-[#FDB515] duration-200 cursor-pointer"
+        key={item}
+        onClick={() => setMobileNav(false)}
+      >
+        {item}
+      </li>
+    );
+  });
 
   return (
     <div className="fixed bg-[#263A5F] flex justify-between items-center w-screen px-4 py-2 h-14 text-white">
       {/* logo */}
       <div>
-        <img src={logo} alt="Logo" className="w-10 h-10" />
+        <img src={logo} alt="Logo" className="w-16 h-16" />
       </div>
 
       {/* nav list */}
-      <ul className="hidden md:flex gap-4 text-xl">
-        <li>Home</li>
-        <li>About</li>
-        <li>Work</li>
-        <li>Contact</li>
-      </ul>
+      <ul className="hidden md:flex gap-6 text-xl">{navList}</ul>
 
       {/* hamburger */}
       <div className="flex md:hidden cursor-pointer z-20" onClick={handleClick}>
-        {mobileNav ? <HiXMark /> : <HiBars3 />}
+        {mobileNav ? (
+          <HiXMark size={30} className="hover:text-red-600" />
+        ) : (
+          <HiBars3 size={30} className="hover:text-[#FDB515]" />
+        )}
       </div>
 
       {/* mobile nav list */}
       <ul
         className={
           mobileNav
-            ? " flex flex-col md:hidden gap-12 text-4xl absolute w-full h-screen bg-[#263A5F] justify-center items-center top-0 left-0 "
+            ? " flex flex-col md:hidden gap-12 text-5xl absolute w-full h-screen bg-[#263A5F] justify-center items-center top-0 left-0"
             : "hidden"
         }
       >
-        <li>Home</li>
-        <li>About</li>
-        <li>Work</li>
-        <li>Contact</li>
+        {mobileNavList}
       </ul>
 
       {/* social icon */}
-        <SocialLinkMenu/>
+      <SocialLinkMenu />
     </div>
   );
 };
