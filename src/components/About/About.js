4510817145b183e00a7f11394e75ed00a7a39968
RukeshB <rukeshbasukala@gmail.com>
2023-08-14 21:26:59 +0545

    update gap between content

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,7 +10,7 @@ const About = () => {
           </div>
           <div></div>
         </div>
-        <div className="max-w-[1000px] grid w-full gap-6 px-4 sm:grid-cols-2">
+        <div className="max-w-[1000px] grid w-full gap-4 sm:gap-6 px-4 sm:grid-cols-2">
           <p className="text-2xl font-bold md:text-4xl sm:text-right">
             Hi. I'm Rukesh Basukala, nice to meet you. Please take a look
             around.
