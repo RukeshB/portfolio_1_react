0e0ca5dcf820f0d621c9f395585f072d49db9c7e
RukeshB <rukeshbasukala@gmail.com>
2023-08-15 23:05:59 +0545

    component for works

---
+++ src/components/Works/Work.js
@@ -0,0 +1,18 @@
+import CodePen from "./CodePen";
+import Projects from "./Projects";
+
+const Works = () => {
+  return (
+    <div className="w-full min-h-screen text-gray-300">
+      <div className="flex flex-col items-center justify-center w-full h-full gap-6">
+        {/* projects */}
+        <Projects/>
+
+        {/* code pen */}
+        <CodePen/>
+      </div>
+    </div>
+  );
+};
+
+export default Works;
