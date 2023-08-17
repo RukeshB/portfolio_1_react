7184eb3284baa652c4f7058b12e7142dd5dff62c
RukeshB <rukeshbasukala@gmail.com>
2023-08-17 21:58:41 +0545

    connect getform.io

--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -1,9 +1,9 @@
 const ContactForm = () => {
     return (
-        <form className="max-w-[1000px] px-8">
-            <input type="text" placeholder="Name" className="input"/>
-            <input type="text" placeholder="Email" className="input"/>
-            <textarea placeholder="Message" rows="10" className="input"/>
+        <form className="max-w-[1000px] px-8" action="https://getform.io/f/da4a294f-57d1-4349-b546-073a8bdc03f7" method="POST">
+            <input name="name" type="text" placeholder="Name" className="input"/>
+            <input name="email" type="text" placeholder="Email" className="input"/>
+            <textarea name="message" placeholder="Message" rows="10" className="input"/>
             <div className="flex items-center justify-center">
                 <button type="submit" className="button"> Sent </button>
             </div>
