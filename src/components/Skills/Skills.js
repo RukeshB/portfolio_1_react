9824c96751f397bbb74f207f43d92465e829f72f
RukeshB <rukeshbasukala@gmail.com>
2023-08-25 00:11:11 +0545

    display skills in tab

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -6,41 +6,53 @@ import js from "../../assets/images/js.png";
 import postgresql from "../../assets/images/postgresql.png";
 import react from "../../assets/images/react.png";
 import tailwindcss from "../../assets/images/tailwindcss.png";
+import { useEffect, useState } from "react";
+import TabButton from "../common/TabButton";
 
 const skillList = [
   {
     logo: html,
     name: "HTML",
+    technology: "Frontend",
   },
   {
     logo: css,
     name: "CSS",
+    technology: "Frontend",
   },
   {
     logo: js,
     name: "JS",
+    technology: "Frontend",
   },
   {
     logo: ruby,
     name: "Ruby",
+    technology: "Backend",
   },
   {
     logo: postgresql,
     name: "PostgreSQL",
+    technology: "Database",
   },
   {
     logo: tailwindcss,
     name: "Tailwind CSS",
+    technology: "Frontend",
   },
   {
     logo: react,
     name: "React",
+    technology: "Frontend",
   },
   {
     logo: rails,
     name: "Ruby on Rails",
+    technology: "Backend",
   },
 ];
+
+const technology = ["All", "Frontend", "Backend", "Database"];
 
 const SkillsCard = (props) => {
   return (
@@ -51,19 +63,47 @@ const SkillsCard = (props) => {
   );
 };
 
-const skillCardList = skillList.map((skill) => {
-  return <SkillsCard logo={skill.logo} name={skill.name} key={skill.name} />;
-});
-
 const Skills = () => {
+  const [selectedTechnology, setSelectedTechnology] = useState("All");
+  const [filteredSkillList, setFilterSkillList] = useState(skillList);
+
+  const handleTecChange = (event) => {
+    setSelectedTechnology(event.target.innerText);
+  };
+
+  useEffect(() => {
+    if (selectedTechnology === "All") {
+      setFilterSkillList(skillList);
+    } else {
+      const filtered = skillList.filter(
+        (skill) => skill.technology === selectedTechnology
+      );
+      setFilterSkillList(filtered);
+    }
+  }, [selectedTechnology]);
+
+  const skillCardList = filteredSkillList.map((skill) => {
+    return <SkillsCard logo={skill.logo} name={skill.name} key={skill.name} />;
+  });
+
+  const tabs = technology.map((tec) => {
+    return (
+      <TabButton
+        key={tec}
+        title={tec}
+        handleClick={handleTecChange}
+        active={tec === selectedTechnology ? true : false}
+      />
+    );
+  });
+
   return (
     <div name="skill" className="w-full min-h-screen">
       <div className="flex flex-col items-center justify-center w-full h-full gap-4">
         <div className="max-w-[1000px]">
-          <h3 className="title">
-            Technical Skills
-          </h3>
+          <h3 className="title">Technical Skills</h3>
         </div>
+        <div className="flex flex-row gap-4 text-xl">{tabs}</div>
         <div className="grid grid-cols-2 gap-4 text-center sm:grid-cols-4 sm:gap-6">
           {skillCardList}
         </div>
